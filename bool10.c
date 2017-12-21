feb5b1dfd2a4412268988591005710a1cbb5405d
sintabocin <32736930+sintabocin@users.noreply.github.com>
2017-12-21 20:42:12 +0700

    Add files via upload

---
+++ bool10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main (){
+
+	bool x = true;
+	bool y = false;
+	bool z = true;
+
+	printf ("%d\n", (x && y) || z);
+	printf ("%d\n", x && y || z);
+	printf ("%d\n", ! x && z);
+	printf ("%d\n",  !x  && !z || !y);
+
+	return 0;
+}
